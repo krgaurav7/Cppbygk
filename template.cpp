07aed1651a1f7c3ac85a7e7d29b1f08cd45dc719
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-11-25 21:08:23 +0530

    Create basic template for C++ program
    Initialize template for competitive programming.

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<long long> vi;
+
+
+int main() {
+	ll t;
+	std::cin >> t;
+	while(t--){
+	    ll n; ll k;
+	    cin >>n;
+	    
+	}
+
+}
